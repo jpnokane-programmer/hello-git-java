9b25597ae37187914ef6c30092fdead363f03fda
jpnokane-programmer <jpnokane@gmail.com>
2026-03-21 22:35:10 +0200

    Update greeting message

--- HelloGit.java
+++ HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[]args){
 
-        System.out.println("Hello Git and Github from Joy");
+        System.out.println("Lovely knowing you");
     }
 }
